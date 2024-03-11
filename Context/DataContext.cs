b7c17e1af4c513c4deefb436dc5ab70bbac539ce
Andrew Chan <chan.andrew1995@gmail.com>
2024-03-10 23:53:52 -0400

    added a few models and a data context and updated readme

---
+++ Context/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using BudgetPlanner.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace BudgetPlanner.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<BudgetItem> BudgetItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ Models/BudgetItem.cs
@@ -0,0 +1,11 @@
+﻿namespace BudgetPlanner.Models
+{
+    public class BudgetItem
+    {
+        public int Id { get; set; }
+        public string item_name { get; set; }
+        public decimal item_budget { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
